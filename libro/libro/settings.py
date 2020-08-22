56357d427894c929e3f96ab4d588a1432e23e9cb
Franky-code <fco.r.frias.valderrama@gmail.com>
2020-08-21 21:54:49 -0500

    Se agrega apps

--- libro/libro/settings.py
+++ libro/libro/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'applications.home',
     'applications.biblioteca',
     #'home.apps.HomeConfig',
     #'biblioteca.apps.BibliotecaConfig',
